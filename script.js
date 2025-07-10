433b63c534ca12235dd0fce4c5696e1b920bf2bb
henragan <31008220+henragan@users.noreply.github.com>
2025-07-10 15:57:30 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function changeMessage() {
-  document.getElementById("message").innerText = "You clicked the button!";
+const correctPassword = "checking you out??";
+
+function checkPassword() {
+  const userInput = document.getElementById("passwordInput").value;
+  const errorText = document.getElementById("error");
+
+  if (userInput === correctPassword) {
+    document.getElementById("lockscreen").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
+  } else {
+    errorText.style.display = "block";
+  }
 }
+
+window.addEventListener("load", () => {
+  document.getElementById("passwordInput").focus();
+});
